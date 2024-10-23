5563c845d8d4240b26de59274b9ee39915a9aeac
RPXCODE <iqodhulhimam2@gmail.com>
2024-10-23 14:03:16 +0700

    update

---
+++ pd_1.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main () {
+	int i;
+    int a = 1;
+    int b = 0;
+    
+    for ( i = 0; i < 6; i++) {
+ 	
+        b += 1; // + 1
+        printf("%d \t", a);
+        a += b; // Output a dari hasil sebelumnya + variable b
+        
+        
+         
+        
+        
+    }
+
+    
+    return 0;
+}
